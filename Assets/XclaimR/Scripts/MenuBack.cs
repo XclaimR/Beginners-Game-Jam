c10c96cab788a232de6b2ae8d9b9ccd502b3de4f
nishanthkorupolu <korupolu.nishanth@gmail.com>
2020-10-14 13:58:03 +0530

    Connections

---
+++ Assets/XclaimR/Scripts/MenuBack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuBack : MonoBehaviour
+{
+    public Text back;
+    private int fontSize;
+    private Color tempColor;
+    private AudioSource audio;
+    //public AudioClip Mysound;
+
+    void Start()
+    {
+        tempColor = back.color;
+        fontSize = back.fontSize;
+        //audio = GetComponent<AudioSource>();
+    }
+
+    void OnMouseEnter()
+    {
+        back.fontSize = fontSize + 5;
+        //audio.Play();
+    }
+
+    void OnMouseExit()
+    {
+        back.fontSize = fontSize;
+    }
+
+
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
